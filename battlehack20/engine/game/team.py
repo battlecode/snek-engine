9b34e5eb5b02484cec19f541df6508ae2254534b
fabianruiz3 <fabian.ruiz3@upr.edu>
2024-10-12 14:22:41 -0400

    add team_info

--- battlehack20/engine/game/team.py
+++ battlehack20/engine/game/team.py
@@ -4,3 +4,9 @@ from enum import Enum
 class Team(Enum):
     WHITE = 0
     BLACK = 1
+
+    def opponent(self):
+        if self == Team.WHITE:
+            return Team.BLACK
+        else:
+            return Team.WHITE
