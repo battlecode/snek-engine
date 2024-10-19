c7006e0d5f6784d866a72eb048be4d5fd330bacc
Maggie Wu <maggiewu@dhcp-10-29-249-45.dyn.MIT.EDU>
2024-10-19 14:18:22 -0400

    robot type

--- battlehack20/engine/game/robottype.py
+++ battlehack20/engine/game/robottype.py
@@ -2,5 +2,19 @@ from enum import Enum
 
 
 class RobotType(Enum):
-    OVERLORD = 0
-    PAWN = 1
+    ### (health, maximum paint stash, paint cost, chip cost, damage to towers, action cost, action cooldown)
+    SOLDIER = (250, 200, 100, 250, 20, 5, 10)
+    SPLASHER = (150, 300, 150, 400, 50, 50, 50)
+    MOPPER = (50, 100, 50, 300, 0, __, 40)
+
+
+    def canAttack(self): 
+        return self == RobotType.SOLDIER or self == RobotType.SPLASHER
+    
+
+    def canMop(self): 
+        return self == RobotType.MOPPER
+
+    
+
+    
