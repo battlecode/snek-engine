b39b45725bb42f2f7a4e458df60112fb0ecd682e
hank <hpstennes@gmail.com>
2025-01-06 16:50:19 -0600

    MapLocation type annotations

--- battlecode25/engine/game/map_location.py
+++ battlecode25/engine/game/map_location.py
@@ -6,33 +6,25 @@ class MapLocation:
         self.x = x
         self.y = y
     
-    def compare_to(self, other):
+    def compare_to(self, other: 'MapLocation') -> int:
         if self.x != other.x:
             return self.x - other.x
         return self.y - other.y
-    
-    def equals(self, other):
-        if not isinstance(other, MapLocation):
-            return False
-        return self.x == other.x and self.y == other.y
-    
-    def to_string(self):
-        return (self.x, self.y)
 
-    def distance_squared_to(self, other):
+    def distance_squared_to(self, other: 'MapLocation') -> int:
         dx = self.x - other.x
         dy = self.y - other.y
         return dx*dx + dy*dy
     
-    def is_within_distance_squared(self, other, distance_squared):
+    def is_within_distance_squared(self, other, distance_squared) -> bool:
         return self.distance_squared_to(other) <= distance_squared
     
-    def is_adjacent_to(self, other):
+    def is_adjacent_to(self, other: 'MapLocation') -> bool:
         dx = self.x - other.x
         dy = self.y - other.y
         return -1 <= dx <= 1 and -1 <= dy <= 1
     
-    def direction_to(self, other):
+    def direction_to(self, other: 'MapLocation') -> Direction:
         if other is None:
             return None
         
@@ -55,19 +47,19 @@ class MapLocation:
             elif dx < 0 and dy < 0:
                 return Direction.SOUTHWEST
 
-    def translate(self, dx, dy):
+    def translate(self, dx: int, dy: int) -> 'MapLocation':
         return MapLocation(self.x + dx, self.y + dy)
 
-    def add(self, dir):
+    def add(self, dir: Direction) -> 'MapLocation':
         return self.translate(dir.value[0], dir.value[1])
     
-    def subtract(self, dir):
+    def subtract(self, dir: Direction) -> 'MapLocation':
         return self.translate(-dir.value[0], -dir.value[1])
     
     def __hash__(self):
         return (self.y + 0x8000) & 0xffff | (self.x << 16)
     
-    def __eq__(self, other):
+    def __eq__(self, other: 'MapLocation'):
         return self.x == other.x and self.y == other.y
     
     def __str__(self):
